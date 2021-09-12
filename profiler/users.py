c4aaaeae3427bb9e3178f32d1e0f6015744991da
Saji Nediyanchath <saji89@gmail.com>
2021-09-13 00:04:18 +0530

    - Implemented change passowrd end-point - Added vscode file to the gitignore.

--- profiler/users.py
+++ profiler/users.py
@@ -1,6 +1,6 @@
 from sqlite3.dbapi2 import Cursor
 from flask import Blueprint, g, jsonify, request
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from .auth import login_required
 from .db import get_db
@@ -101,6 +101,37 @@ def update_user(user_id):
         db.commit()
     except db.IntegrityError:
         return {"error": "This email is already registered"}, 400
+
+    return jsonify({"id": user_id}), 200
+
+@blueprint.route("/<int:user_id>/change_password", methods=["PUT"])
+@login_required
+def change_password(user_id):
+    if user_id != g.user["id"]:
+        return {"error": "Permission denied"}, 401
+
+    data = request.json
+
+    current_password = data.get("current_password")
+    new_password = data.get("new_password")
+
+    if not all([current_password, new_password]):
+        return {"error": "Required fields are missing"}, 400
+
+    db = get_db()
+    cursor = db.execute("SELECT * FROM user where id=?", (user_id,))
+    user = cursor.fetchone()
+
+    if not check_password_hash(user["password"], current_password):
+        return {"error": "Incorrect current password"}, 400
+
+    hashed_password = generate_password_hash(new_password)
+
+    db.execute(
+        "UPDATE user SET password=? WHERE id=?",
+        (hashed_password, user_id),
+    )
+    db.commit()
 
     return jsonify({"id": user_id}), 200
 
